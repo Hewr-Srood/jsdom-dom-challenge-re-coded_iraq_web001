28fc0d357793ac787aad6cd13924f751812e8612
Hewr Srood <hewr_s@outlook.com>
2020-05-31 18:01:53 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -45,7 +45,26 @@ function stop(){
     disableBtns(false);
   }
 }
+ function heartFunc () {
+    var a = document.getElementById("counter"),
+      b = parseInt(a.innerText),
+      c = document.querySelector(".likes"),
+      d = void 0;
+    if ([].concat(_toConsumableArray(c.children)).map(function (a) 
+    {
+      return parseInt(a.dataset.num);
+    })
+        .includes(b)
+    ) {
+      d = document.querySelector('[data-num="' + b + '"]');
+      var e = parseInt(d.children[0].innerText);
+      d.innerHTML = b + " has been liked <span>" + (e + 1) + "</span> times";
+    } else (d = document.createElement("li")).setAttribute("data-num", b), (d.innerHTML = b + " has been liked <span>1</span> time"), c.appendChild(d);
+  }
 
+
+
+heart.addEventListener('click',heartFunc);
 plus.addEventListener('click',increment);
 minus.addEventListener('click',decrement);
 pause.addEventListener('click',stop)
